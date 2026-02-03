99cfce336fc4ea307ae78c11919a4aaa30a1f317
Jason Dick <Jason.L.Dick@gmail.com>
2026-02-03 20:01:59 +0100

    Getting hung up on #app section. A bit confused about props. Will study and come back.

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,32 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  if (!show) {
+    return (
+      <div className="show-details">
+        <p>No show selected! Select a show!</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        /* show only contains name, episodes and genre
+        selectedEpisode and setSelectedEpisode are my variables
+        not variables from show.. */
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
